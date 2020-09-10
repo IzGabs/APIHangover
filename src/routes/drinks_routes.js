91e11c3d739f3678c6e3501e53efcf3e4839b54f
Gabriel Medeiros <gabrielop1ece@gmail.com>
2020-09-09 21:32:24 -0300

    Push para exorcisar

--- src/routes/drinks_routes.js
+++ src/routes/drinks_routes.js
@@ -2,9 +2,7 @@ const drinskRouter = require('express').Router();
 const controllerDrinks = require('../controllers/Drink_Controlller')()
 const authToken = require('../controllers/app_controller')()
 
-drinskRouter.get('/Listar',
- //authToken.authenticateToken,
-  controllerDrinks.listarDrinks)
+drinskRouter.get('/Listar', authToken.authenticateToken, controllerDrinks.listarDrinks)
 
 drinskRouter.get('/Listar/:id', controllerDrinks.listarDrinksById);
 
