cc84342e3cb42f185e7e0f59123023c3db5a7e50
Rokol <biel_s.carvalho2@hotmail.com>
2020-09-12 21:23:19 -0300

    ge

--- src/routes/drinks_routes.js
+++ src/routes/drinks_routes.js
@@ -2,18 +2,24 @@ const drinskRouter = require('express').Router();
 const controllerDrinks = require('../controllers/Drink_Controlller')()
 const authToken = require('../controllers/app_controller')()
 
-drinskRouter.get('/Listar', authToken.authenticateToken, controllerDrinks.listarDrinks)
+drinskRouter.get('/Listar', 
+//authToken.authenticateToken, 
+controllerDrinks.listarDrinks)
 
-drinskRouter.get('/Listar/:id', controllerDrinks.listarDrinksById);
+drinskRouter.get('/Listar/:id', 
+//authToken.authenticateToken, 
+controllerDrinks.listarDrinksById);
 
-drinskRouter.post('/adicionar', controllerDrinks.add);
+drinskRouter.post('/adicionar', 
+//authToken.authenticateToken, 
+controllerDrinks.add);
 
-drinskRouter.put('/update/:id', controllerDrinks.update);
+drinskRouter.put('/update/:id',  
+//authToken.authenticateToken, 
+controllerDrinks.update);
 
-drinskRouter.delete('/delete/:id', controllerDrinks.delete);
+drinskRouter.delete('/delete/:id', 
+//authToken.authenticateToken,
+controllerDrinks.delete);
 
 module.exports = drinskRouter;
-
-/*
-
-*/
